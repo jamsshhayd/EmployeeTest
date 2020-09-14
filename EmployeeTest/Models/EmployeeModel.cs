e8d94d3b36c0654df93457c80052c86af9d26174
Jamshayd Kamran <jamshaid@neurun.ca>
2020-09-15 00:40:10 +0500

    Added Employee Models

---
+++ EmployeeTest/Models/EmployeeModel.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeTest.Models
+{
+    public class EmployeeModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("employee_name")]
+        public string EmployeeName { get; set; }
+
+        [JsonProperty("employee_salary")]
+        public long EmployeeSalary { get; set; }
+
+        [JsonProperty("employee_age")]
+        public long EmployeeAge { get; set; }
+
+        [JsonProperty("profile_image")]
+        public string ProfileImage { get; set; }
+    }
+}
---
+++ EmployeeTest/Models/EmployeeResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeTest.Models
+{
+    public class EmployeeResponseModel
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("data")]
+        public List<EmployeeModel> Data { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
